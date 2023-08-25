7a9e973dc7697faa2e33ca290746ea7dbf6463ba
Subbu <subramaniank@icloud.com>
2023-08-25 09:33:21 -0400

    Update giter.sh

--- giter.sh
+++ giter.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Step 1: Request the clone URL
-read -p "Enter the clone URL for the Git repo: " clone_url
+# Check if a default clone URL is set as an argument
+if [ $# -eq 1 ]; then
+    clone_url="$1"
+else
+    # Request the clone URL
+    read -p "Enter the clone URL for the Git repo: " clone_url
+fi
 
 # Step 2: Clone the repo and checkout the latest master branch
 git clone "$clone_url"
-cd "$(basename "$clone_url" .git)"
+repo_name=$(basename "$clone_url" .git)
+cd "$repo_name"
 git checkout master
 
 # Step 3: Get user information
@@ -15,8 +21,14 @@ fullName=$(id -F)
 # Step 4: Append user information to Readme.MD
 echo -e "\n$fullName <$userName>" >> Readme.MD
 
-# Step 5: Create and checkout the new branch
-branch_name="onboard/$userName"
+# Step 5: Create and checkout the new branch with rolling number
+base_branch_name="onboard/$userName"
+branch_name="$base_branch_name"
+counter=1
+while git rev-parse --verify "$branch_name" &>/dev/null; do
+    counter=$((counter + 1))
+    branch_name="$base_branch_name"_"$counter"
+done
 git checkout -b "$branch_name"
 
 # Step 6: Commit changes
